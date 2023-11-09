d42798e4647a56533d5ad2cb91a1b35cf3557292
nurislamkalmanbetov <kalmanbetovnurislam19@gmail.com>
2023-11-09 12:00:16 +0600

    add psql, alembic with migrations to psql

---
+++ lesson_4/models/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import MetaData, Integer, String, Table, Column, JSON, TIMESTAMP, ForeignKey
+from datetime import datetime
+
+
+metadata = MetaData()
+
+roles = Table(
+    "roles",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String, nullable=False),
+    Column("permissions", JSON),
+)
+
+
+users = Table(
+    "users",
+    metadata, 
+    Column("id", Integer, primary_key=True),
+    Column("email", String, nullable=False),
+    Column("username", String, nullable=False),
+    Column("password", String, nullable=False),
+    Column("registered_at", TIMESTAMP, default=datetime.utcnow()),
+    Column("role_id", Integer, ForeignKey("roles.id")),
+)
+
+
